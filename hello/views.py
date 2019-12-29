856adf729bf99dfa34a7ecab5f2e57a766214b6b
Benjamin Mende <bmende@moosehill.com>
2019-12-29 09:10:42 -0800

    add some static files of sheba. fix the html to make more sense for this project

--- hello/views.py
+++ hello/views.py
@@ -7,10 +7,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+
+    print(request)
+    return render(request, "index.html")
 
 
 
